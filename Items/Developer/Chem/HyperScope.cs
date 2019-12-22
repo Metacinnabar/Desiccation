f8149f5b68d4584cd03688d1961ac10f92cd8ff5
charlie brannan <charliebrannan154@gmail.com>
2019-12-22 14:13:07 +1100

    chem dev item changes

--- Items/Developer/Chem/HyperScope.cs
+++ Items/Developer/Chem/HyperScope.cs
@@ -1,4 +1,4 @@
-using Microsoft.Xna.Framework;
+using Desiccation.Projectiles.Developer.Chem;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -7,61 +7,62 @@ namespace Desiccation.Items.Developer.Chem
 {
 	public class HyperScope : ModItem
 	{
-		public override void SetStaticDefaults() 
+		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("Rapidly fires energy shots in bursts of three\nWhen low on health, fires slow but powerful orbs\n'Destroy them with lazers!'");
+			Tooltip.SetDefault("Rapidly fires energy shots in bursts of three\nWhen low on health, fires slow but powerful orbs\n'Destroy them with lazers!'\nChem's Developer Weapon!");
 		}
 
-        public override void SetDefaults()
-        {
-            item.magic = true;
-            item.width = 88;
-            item.height = 34;
-            item.damage = 35;
-            item.useTime = 5;
-            item.useAnimation = 15;
-            item.mana = 14;
-            item.UseSound = SoundID.Item91;
-            item.shoot = mod.ProjectileType("SuperShot");
-            item.shootSpeed = 20f;
-            item.useStyle = 5;
-            item.knockBack = 3;
-            item.value = 1000000;
-            item.rare = 5;
-            item.autoReuse = true;
-        }
-
-        public override void HoldItem(Player player)
-        {
-            if (player.statLife <= player.statLifeMax2 / 5) //statLifeMax2 is used instead of statLifeMax since the latter is only the base max life value
-            {
-                item.damage = 175;
-                item.useTime = 30;
-                item.useAnimation = 30;
-                item.mana = 60;
-                item.shoot = mod.ProjectileType("HyperShot");
-                item.shootSpeed = 15f;
-            }
-            else
-            {
-                item.damage = 35;
-                item.useTime = 5;
-                item.useAnimation = 15;
-                item.mana = 14;
-                item.shoot = mod.ProjectileType("SuperShot");
-                item.shootSpeed = 20f;
-            }
-        }
-
-        public override void AddRecipes() 
+		public override void SetDefaults()
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.LaserRifle);
-            recipe.AddIngredient(ItemID.SoulofSight, 20);
-            recipe.AddIngredient(ItemID.IllegalGunParts);
-            recipe.AddTile(TileID.MythrilAnvil);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			item.magic = true;
+			item.width = 88;
+			item.height = 34;
+			item.damage = 35;
+			item.useTime = 5;
+			item.useAnimation = 15;
+			item.mana = 14;
+			item.UseSound = SoundID.Item91;
+			item.shoot = ModContent.ProjectileType<SuperShot>();
+			item.shootSpeed = 20f;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.knockBack = 3;
+			//Restat the value please. thx
+			item.value = 1000000;
+			item.rare = ItemRarityID.Pink;
+			item.autoReuse = true;
+		}
+
+		public override void HoldItem(Player player)
+		{
+			if (player.statLife <= player.statLifeMax2 / 5)
+			{
+				item.damage = 175;
+				item.useTime = 30;
+				item.useAnimation = 30;
+				item.mana = 60;
+				item.shoot = ModContent.ProjectileType<HyperShot>();
+				item.shootSpeed = 15f;
+			}
+			else
+			{
+				item.damage = 35;
+				item.useTime = 5;
+				item.useAnimation = 15;
+				item.mana = 14;
+				item.shoot = ModContent.ProjectileType<SuperShot>();
+				item.shootSpeed = 20f;
+			}
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe r = new ModRecipe(mod);
+			r.AddIngredient(ItemID.LaserRifle);
+			r.AddIngredient(ItemID.SoulofSight, 12);
+			r.AddIngredient(ItemID.IllegalGunParts);
+			r.AddTile(TileID.MythrilAnvil);
+			r.SetResult(this);
+			r.AddRecipe();
 		}
 	}
 }

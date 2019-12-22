f928e4618228424641d60d1e298b947fead03df7
Chem <minecartanimation@gmail.com>
2019-12-21 23:26:49 -0700

    Revert "Hyper Scope low health boost rebalancing"
    This reverts commit 8223734b3cc2afcdef1bc7ec253bfade8b8bb850.

--- Items/Developer/Chem/HyperScope.cs
+++ Items/Developer/Chem/HyperScope.cs
@@ -1,18 +1,15 @@
 using Desiccation.Projectiles.Developer.Chem;
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.DataStructures;
 
 namespace Desiccation.Items.Developer.Chem
 {
 	public class HyperScope : ModItem
 	{
-        bool TookLife;
 		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("Rapidly fires energy shots in bursts of three\nOvercharges at critical health, firing slow but powerful orbs\nConsumes life when used while overcharged\n'Destroy them with lazers!'");
+			Tooltip.SetDefault("Rapidly fires energy shots in bursts of three\nWhen low on health, fires slow but powerful orbs\n'Destroy them with lazers!'");
 		}
 
 		public override void SetDefaults()
@@ -34,46 +31,29 @@ namespace Desiccation.Items.Developer.Chem
 			item.autoReuse = true;
 		}
 
-        public override void HoldItem(Player player)
-        {
-            if (player.statLife <= player.statLifeMax2 / 5)
-            {
-                if (player.releaseUseItem)
-                    TookLife = false;
-                if (player.statLife < 0)
-                    player.KillMe(PlayerDeathReason.ByCustomReason(player.name + " couldn't contain the watts."), player.statLife, 0);
-                if (Main.rand.Next(3) == 0)
-                    Dust.NewDust(player.position, player.width, player.height, DustID.Electric, 0, 0, 0, Color.White, 0.75f);
-                item.damage = 175;
-                item.useTime = 30;
-                item.useAnimation = 30;
-                item.mana = 60;
-                item.shoot = ModContent.ProjectileType<HyperShot>();
-                item.shootSpeed = 15f;
-            }
-            else
-            {
-                item.damage = 35;
-                item.useTime = 5;
-                item.useAnimation = 15;
-                item.mana = 14;
-                item.shoot = ModContent.ProjectileType<SuperShot>();
-                item.shootSpeed = 20f;
-            }
-        }
+		public override void HoldItem(Player player)
+		{
+			if (player.statLife <= player.statLifeMax2 / 5)
+			{
+				item.damage = 175;
+				item.useTime = 30;
+				item.useAnimation = 30;
+				item.mana = 60;
+				item.shoot = ModContent.ProjectileType<HyperShot>();
+				item.shootSpeed = 15f;
+			}
+			else
+			{
+				item.damage = 35;
+				item.useTime = 5;
+				item.useAnimation = 15;
+				item.mana = 14;
+				item.shoot = ModContent.ProjectileType<SuperShot>();
+				item.shootSpeed = 20f;
+			}
+		}
 
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            if (player.statLife <= player.statLifeMax2 / 5 && !TookLife)
-            {
-                TookLife = true;
-                player.statLife -= 5;
-                CombatText.NewText(player.getRect(), CombatText.DamagedFriendly, 5);
-            }
-            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
-        }
-
-        public override void AddRecipes()
+		public override void AddRecipes()
 		{
 			ModRecipe r = new ModRecipe(mod);
 			r.AddIngredient(ItemID.LaserRifle);
